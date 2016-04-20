0f82970a8ad7041ebcc7e00db524f431b889b1a4
Doug McInnes <doug@dougmcinnes.com>
2016-04-19 20:52:47 -0700

    wavy waves

--- visualize.js
+++ visualize.js
@@ -3,21 +3,24 @@ var stats = new Stats();
 var context;
 var offset = 0;
 
+var WAVE_COUNT = 20;
+
 var TAU = 2 * Math.PI
 
 var waves = [];
 
 var Wave = {
   calc: function (theta) {
-    return Math.sin((theta / (TAU * this.frequency)) - this.phase) * this.amplitude;
+    return Math.sin((theta / (TAU * this.frequency)) - this.phase);
   }
 };
 
-for (var i = 0; i < 10; i++) {
+for (var i = 0; i < WAVE_COUNT; i++) {
   waves.push(
     Object.assign({
       amplitude: Math.random() * 100 + 50,
       phase:     Math.random() * TAU,
+      phaseVel:  Math.random() * 2 - 1,
       offset:    Math.random() * 400 - 200,
       frequency: 10
     }, Wave)
@@ -29,10 +32,10 @@ var render = function () {
   var width = context.canvas.width;
   var height = context.canvas.height;
   context.clearRect(0, 0, width, height);
-  for (var i = 0; i < waves.length; i++) {
+  for (var i = 0; i < WAVE_COUNT; i++) {
     var wave = waves[i];
     for (var x = 0; x < width; x++) {
-      var y = (height/2) - wave.calc(x) + wave.offset;
+      var y = (height/2) - wave.calc(x) * wave.amplitude + wave.offset;
       context.fillStyle = 'lightgray';
       context.fillRect(x, y, 3, 3);
     }
@@ -53,9 +56,11 @@ var tick = function (timestamp) {
 };
 
 var step = function (delta) {
-  for (var i = 0; i < waves.length; i++) {
+  for (var i = 0; i < WAVE_COUNT; i++) {
     var wave = waves[i];
-    wave.phase = (wave.phase + (delta/2000)) % TAU;
+    var nextWave = waves[i + 1] || waves[0]; // wrap
+    wave.phase = (wave.phase + (wave.phaseVel * delta/2000)) % TAU;
+    wave.amplitude = wave.amplitude + (nextWave.calc(wave.amplitude) / TAU);
   }
 };
 
